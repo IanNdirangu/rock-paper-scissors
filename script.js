0fd6487f2c30ab0e41240091c588a2a984c3330f
IanNdirangu <puvandeerang@gmail.com>
2024-06-07 20:43:43 +0300

    Created the respective html, css and js files and added to repo

---
+++ script.js
@@ -0,0 +1,22 @@
+// First we'll check if “Hello World” is logged in the browser console once you open your webpage.
+console.log("Hello World");
+
+// Initialize the Human and Computer Scores
+let humanScore = 0;
+let computerScore = 0;
+
+// For the Human and Computer Choices
+let humanChoice = "";
+let computerChoice = "";
+
+
+
+// Create a function to get the Computer Choice
+function getComputerChoice() {
+
+}
+
+// Create a function to get the Human Choice
+function getHumanChoice() {
+
+}
